c7bee8f4238f5049d2c7f35d8bcfa1a4eb734316
sawada <sawadasuiren@gmail.com>
2015-07-13 22:42:22 +0900

    app.js内をAppクラスに書き直し

--- src/app.js
+++ src/app.js
@@ -3,71 +3,65 @@ import * as Util from './js/utility.js';
 import Map from './js/map.js';
 import Avatar from './js/avatar.js';
 
-var stage, map, myAvatar;
-var others = [];
+class App {
+    constructor(params) {
+        
+        // ステージの準備
+        var canvasElement = document.getElementById("my-canvas");
+        this.stage = new createjs.Stage(canvasElement);
 
+        // マップ生成
+        this.map = new Map();
+        this.stage.addChild(this.map);
 
-function initialize() {
-    // canvasタグを取得
-    var canvasElement = document.getElementById("my-canvas");
-    // ステージの準備
-    stage = new createjs.Stage(canvasElement);
+        // アバターのリスト
+        this.avatarList = [];
 
-    // マップ生成
-    map = new Map();
-    stage.addChild(map);
+        // 自分のアバター
+        this.myAvatar = new Avatar({
+            cellX: 1,
+            cellY: 1,
+            image: '/img/pokemonrgb_various_sheet.png'
+        });
+        this.map.addChild(this.myAvatar);
 
-    // 自分のアバター
-    myAvatar = new Avatar({
-        cellX: 1,
-        cellY: 1,
-        image: '/img/pokemonrgb_various_sheet.png'
-    });
-    map.addChild(myAvatar);
+        // 自分のアバターをリストに追加
+        this.avatarList.push(this.myAvatar);
 
-    // キーボード入力を監視
-    document.onkeydown = (e) => {
-        var _dir = null;
-        var _walkDisable = false;
-        var nowX = myAvatar.getCellX(),
-            nowY = myAvatar.getCellY();
-        switch(e.keyCode) {
-            case 38: 
-                _dir = 'up';
-                _walkDisable = map.validateCell(nowX, nowY - 1);
-                break;
-            case 37:
-                _dir = 'left';
-                _walkDisable = map.validateCell(nowX - 1, nowY);
-                break;
-            case 39:
-                _dir = 'right';
-                _walkDisable = map.validateCell(nowX + 1, nowY);
-                break;
-            case 40:
-                _dir = 'down';
-                _walkDisable = map.validateCell(nowX, nowY + 1);
-                break;
-            default: return;
-        }
-        myAvatar.walk(_dir, _walkDisable);
-    };
+        // キーボード入力を監視
+        document.onkeydown = (e) => {
+            var _dir = null;
+            var _walkDisable = false;
+            var nowX = this.myAvatar.getCellX(),
+                nowY = this.myAvatar.getCellY();
+            switch(e.keyCode) {
+                case 38: 
+                    _dir = 'up';
+                    break;
+                case 37:
+                    _dir = 'left';
+                    break;
+                case 39:
+                    _dir = 'right';
+                    break;
+                case 40:
+                    _dir = 'down';
+                    break;
+                default: return;
+            }
+            this.myAvatar.walk(_dir);
+        };
 
-    // キーボード入力終了を監視
-    document.onkeyup = (e) => {
-        myAvatar.stopWalk();
-    };
+        // キーボード入力終了を監視
+        document.onkeyup = (e) => {
+            this.myAvatar.stopWalk();
+        };
 
-    // 1コマ毎にupdate
-    createjs.Ticker.addEventListener("tick", () => {
-        stage.update();
-    });
+        // 1コマ毎にupdate
+        createjs.Ticker.addEventListener("tick", () => {
+            this.stage.update();
+        });
+    }
 }
 
-
-function addOtherAvatar() {
-
-}
-
-
-initialize();
+new App();
